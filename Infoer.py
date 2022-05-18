e1a7f8e7000e8259913c4c4653a3a8fccb88c13e
mellonishaparak <mellonishaparak@gmail.com>
2022-05-18 18:20:42 +0430

    .

---
+++ Infoer.py
@@ -0,0 +1,91 @@
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+urlIndex = int(sys.argv[1])
+
+urls = ["https://news.ycombinator.com",
+        "https://www.tomshardware.com",
+        "https://www.howtogeek.com"
+       ]
+
+URL = urls[urlIndex-1]
+
+
+
+page = requests.get(URL)
+soup = BeautifulSoup(page.text, "html.parser")
+
+
+def HackerNews(parsedPage):
+    print("")
+    print("    The Latest News of Hacker News    ")
+    print("")
+    table = parsedPage.find("table",id="hnmain")
+      
+    titleLink = table.find_all("a", class_="titlelink")
+      
+    for link in titleLink:
+        print()
+        print("")
+        print(" Title: ",link.text)
+        print(" Link:  ",link["href"])
+        print("#")
+          
+def HardwareNews(parsedPage):
+    print()
+    print("    The Latest News abou Hardware    ")    
+    print()
+    content = parsedPage.find("div",id="content")
+    
+    titleLink = content.find_all("a", class_="article-link")
+
+    for link in titleLink:
+        print()
+        print("")
+        print(" Title: ", link["aria-label"])
+        print(" Link: ", link["href"])
+        print()
+
+def HowToGeek(parsedPage):
+
+    print("    The Latest News of How to Geek    ")
+
+    main = parsedPage.find("main",id="main")
+
+    articles = main.find_all("article", class_="post")
+    for article in articles:
+        aTag = article.find("a")
+        href = aTag["href"]
+        header = article.find("header")
+        headerText = header["aria-label"]
+        print()
+        print(" Title: ", headerText)
+        print(" Link: ", href)
+        print()
+   
+
+
+def main():
+     
+
+
+    if int(urlIndex-1) == 0:
+        HackerNews(soup)        
+
+    elif int(urlIndex-1) == 1:
+        HardwareNews(soup)
+    
+    elif int(urlIndex-1) == 2:
+        HowToGeek(soup)
+
+    else:
+        print("Invalid Input")
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+  
---
+++ infoer.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#This program give us latest news of technology 
+
+DELAY=1 #Number of seconds to display results
+arr=('-' '\' '|' '/')
+
+spinner(){
+
+while true; do
+    for c in "${arr[@]}"; do
+        echo -en "\r $c "
+        sleep .5
+    done
+    break
+done
+}
+
+
+
+
+main() {
+
+cat << EOF 
+
+    What would you like to read today King *Mani*?
+
+    1.Hacker News
+    2.Hardware News
+    3.How To Geek
+    4.Quit: 
+EOF
+read Sel
+
+spinner
+
+if [ $Sel = 4 ];then
+    return 1
+
+else
+
+    python3 /home/rose/infoer/Infoer.py $Sel | more 
+fi
+
+echo "  Would You Like To Continue My Lord: [y/n] "
+echo ""
+read Continue
+
+if [ $Continue = 'y' ];then
+     main
+
+else
+    echo " Hope to See You Again My Lord"
+    return 1
+fi
+}
+
+main
+
+
+
+
+
+
+
+
